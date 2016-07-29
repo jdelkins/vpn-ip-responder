3e5d4e2cbb6a0894ead61fc3939223a961895b5f
Joel D. Elkins <joel@elkins.co>
2016-07-29 11:19:32 -0500

    add vpn_port_rpcserver for docker container use

--- local_updates.py
+++ local_updates.py
@@ -5,15 +5,18 @@ import subprocess
 import urllib.parse, urllib.request
 import json
 
-def vpn_ip_changed(local_ip):
-    print('reading PIA credentials')
+def get_forwarded_port(local_ip, cred_filename, client_id_filename):
     # read privateinternetaccess credentials
-    cred_file = open('/etc/openvpn/pia.cred', 'r')
+    print('reading PIA credentials from {}'.format(cred_filename))
+    cred_file = open(cred_filename, 'r')
     [user_name, passwd] = [l.strip() for l in cred_file.readlines()]
+    cred_file.close()
+
     # client id is an id number to identify this host within my account
-    client_id_file = open('/etc/openvpn/pia.client_id', 'r')
+    client_id_file = open(client_id_filename, 'r')
     client_id = client_id_file.readline().strip()
-    print('using client_id: {}'.format(client_id))
+    print('got client_id {} from {}'.format(client_id, client_id_filename))
+    client_id_file.close()
 
     # set up the port forward request to PIA
     print('requesting forwarding port from PIA')
@@ -23,38 +26,40 @@ def vpn_ip_changed(local_ip):
 
     # read the response, hopefully it worked. The form is as follows:
     # {"port":37542}
-    port = 6112
+    port = 0
     with urllib.request.urlopen(request) as response:
         j = response.read().decode('utf-8')
         port = int(json.loads(j)['port'])
         print('success: PIA gives us {:d}'.format(port))
-    port_change(port)
+    return port
 
-def port_change(port):
+port_filename = 'forwarded.port'
+def vpn_port_changed(port, config_dir):
     # write the port to a file if it has changed.
-    port_file = open('/etc/openvpn/portforward', 'r')
+    port_filename = os.path.join(config_dir, port_filename)
     oldport = 0
     try:
+        port_file = open(port_filename, 'r')
         oldport = int(port_file.readline().strip())
-    except:
+    except Exception as e:
+        print('Error: problem reading {}: {}'.format(port_filename, e))
+        print('Continuing anyway')
         pass
-    port_file.close()
+    finally:
+        port_file.close()
 
     if oldport == port:
-        print('port {} is unchanged from cached value. stopping'.format(port))
-        return
-
-    print('port changed from cached value ({}). updating cache'.format(oldport))
-    port_file = open('/etc/openvpn/portforward', 'w')
-    port_file.write('{}\n'.format(port))
-    port_file.close()
+        print('Note: port {} is unchanged from cached value.'.format(port))
+    else:
+        print('Note: new port {} is different from cached value of {}.'.format(port, oldport))
+        port_file = open(port_filename, 'w')
+        port_file.write('{}\n'.format(port))
+        port_file.close()
 
     # also  update deluged's port if it is running
-    rc = subprocess.run(['systemctl', 'is-active', 'deluged.service'], stdout=subprocess.PIPE, universal_newlines=True)
-    if rc.stdout.strip() == 'active':
-        print('updating deluge configuration')
-        deluge_rc = subprocess.run(['deluge-console', '-c', '/srv/mediad/.config/deluge', 'config -s listen_ports ({:d}, {:d})'.format(port, port)])
-        deluge_rc.check_returncode()
+    print('Updating deluge configuration')
+    deluge_rc = subprocess.run(['deluge-console', '-c', config_dir, 'config -s listen_ports ({:d}, {:d})'.format(port, port)])
+    deluge_rc.check_returncode()
 
 if __name__ == '__main__':
     vpn_ip_changed(sys.argv[1])
